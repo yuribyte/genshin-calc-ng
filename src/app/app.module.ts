2967cb2f34d6bec667758f0d330bbe39484cc341
Yuri Vitor <yuri.lins25@gmail.com>
2021-01-25 16:38:06 -0300

    v2 release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 
+import { IMaskModule } from 'angular-imask';
+
 import { AppComponent } from './app.component';
+import { CalcComponent } from './calc/calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import * as dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+
+@Component({
+  selector: 'genshin-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [style({ opacity: 0 }), animate(500, style({ opacity: 1 }))]),
+      transition(':leave', [animate(500, style({ opacity: 0 }))])
+    ])
+  ]
+})
+export class CalcComponent implements OnInit {
+
+  static isGreater(group: AbstractControl): { [key: string]: boolean } {
+
+    const current = group.get('currentResin');
+    const expected = group.get('expectedResin');
+
+    if (!current || !expected) {
+      return undefined;
+    }
+
+    if (expected.value > 0) {
+      if (current.value == expected.value) {
+        return { isSame: true };
+      }
+      if (current.value > expected.value) {
+        return { isGreater: true };
+      }
+    }
+
+    return undefined;
+
+  }
+
+  currentResinText = 'Resina atual'
+  expectedResinText = 'Resina esperada'
+  btnText = 'Calcular'
+
+  calculatedResinMinutes;
+  calculatedResinHours;
+  calculatedResinData;
+
+  currentLocalMinutes;
+  currentLocalHours;
+  currentLocalData;
+
+  form: FormGroup;
+
+  result = false
+  same = false
+
+  mask = {
+    mask: Number
+  }
+
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      currentResin: [null, [Validators.required]],
+      expectedResin: [null, [Validators.required]]
+    },
+      { validator: CalcComponent.isGreater }
+    );
+
+    this.currentLocalMinutes = localStorage.getItem('currentLocalMinutes')
+    this.currentLocalHours = localStorage.getItem('currentLocalHours')
+    this.currentLocalData = localStorage.getItem('currentLocalData')
+  }
+
+  calcResin() {
+    const equivalentMinutes = ((parseInt(this.form.value.currentResin) - parseInt(this.form.value.expectedResin)) * 8) * -1
+    const equivalentHours = (equivalentMinutes / 60).toFixed(2)
+    const today = new Date();
+
+    const estimatedCharge = dayjs(today).add(equivalentMinutes, 'm').toDate();
+    const equivalentData = dayjs(estimatedCharge).locale('pt-br').format("dddd, HH:mm")
+
+    this.calculatedResinMinutes = equivalentMinutes
+    this.calculatedResinHours = equivalentHours
+    this.calculatedResinData = equivalentData
+
+    if (this.calculatedResinMinutes > 0) {
+      localStorage.setItem('currentLocalMinutes', this.calculatedResinMinutes)
+    }
+    if (this.calculatedResinHours > 0) {
+      localStorage.setItem('currentLocalHours', this.calculatedResinHours)
+      localStorage.setItem('currentLocalData', this.calculatedResinData)
+    }
+
+
+    this.result = true
+
+  }
+
+}
